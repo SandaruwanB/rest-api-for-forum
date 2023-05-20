effb74e37e29bbcfeb8e268451b6337a74cb7bab
SandaruwanB <sandarusbandara110@gmail.com>
2023-05-20 20:57:58 +0530

    dev commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 
+app.use('/', require('./routes/routes'));
+
 app.listen(8080, ()=>{
     console.log("App started on http://localhost:8080");
 })
