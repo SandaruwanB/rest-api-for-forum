8d0c5ed02d963b4de4108bc2e6070e1f9f2c591d
SandaruwanB <sandarusbandara110@gmail.com>
2023-05-29 08:45:10 +0530

    category model

--- models/categories.js
+++ models/categories.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const dbConnection = require('../database/connection');
 
 const categorySchema = new mongoose.Schema({
-    
+    name : {
+        type : String,
+        require : true,
+    }
 });
 
 const category = mongoose.model("category", categorySchema);
