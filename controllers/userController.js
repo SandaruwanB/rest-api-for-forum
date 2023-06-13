5c5fa1c746a32eb82fe1619f5a31420366e17338
SandaruwanB <sandarusbandara110@gmail.com>
2023-06-13 21:37:24 +0530

    change settings

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const users = require('../models/userModel');
+
+dotenv.config({path : '../config.env'});
+
+module.exports.changePassword = (req,res)=>{
+    const email = req.body.email;
+    const oldPass = req.body.oldPass;
+    const newPass = req.body.newPass;
+
+    users.findOne({email : email}).then(user=>{
+        bcrypt.compare(oldPass, user.password).then(result=>{
+            if(result){
+                bcrypt.genSalt(10, ((err,salt)=>{
+                    bcrypt.hash(newPass, salt, (err, hashPass)=>{
+                        users.findOneAndUpdate({email : email}, {$set : {password : hashPass}}).then(()=>{
+                            res.json({result : 'success'});
+                        })
+                    })
+                }))
+            }
+            else{
+                res.json({result : 'pass'});
+            }
+        })
+    })
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const { signIn, signUp, sendVerificationKey, passwordReset, verify } = require("../controllers/authController");
+const { changePassword } = require("../controllers/userController");
 
 route.get("/", (req,res)=>{
     res.send("working");
@@ -10,6 +11,7 @@ route.post('/login', signIn);
 route.post('/register', signUp);
 route.post('/sendverify', sendVerificationKey);
 route.post('/verify', verify);
+route.post('/pass', changePassword);
 route.post('/test', passwordReset);
 
 
